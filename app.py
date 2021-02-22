8e8e08348018a09d20f45bf656ca99df25ab6138
daisybutler <daisybutler16@gmail.com>
2021-02-22 19:27:27 +0000

    Changed homepage buttons to anchors and added url_for paths.

--- app.py
+++ app.py
@@ -43,6 +43,11 @@ def signup():
     return render_template("/signup.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 @app.route("/logout")
 def logout():
     return render_template("/logout.html")
