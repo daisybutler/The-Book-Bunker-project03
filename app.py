4d5a4d44f1de20b37344152d3d9fbe4b044ee657
daisybutler <daisybutler16@gmail.com>
2021-02-23 20:30:08 +0000

    Added logout functionality.

--- app.py
+++ app.py
@@ -87,7 +87,10 @@ def login():
 
 @app.route("/logout")
 def logout():
-    return render_template("logout.html")
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
