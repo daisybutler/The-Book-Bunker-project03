79b7347d063c8dc01cb458b4c2c0a153c1a47b92
daisybutler <daisybutler16@gmail.com>
2021-02-28 15:37:01 +0000

    Added toggleClass method to .heart with JQuery to swtich between outline and filled on user click.

--- static/js/script.js
+++ static/js/script.js
@@ -4,22 +4,14 @@ $(document).ready(function(){
     $('select').formSelect();
     $('.fixed-action-btn').floatingActionButton();
     $('.tooltipped').tooltip();
-    $('.modal').modal();
+    /* Shows and hides the 'Show All Books' button on 'All Books' 
+    page depending on if the user has filtered results with search bar */
     $('#show-all-books-btn').click(function(){
         $('#show-all-books-btn').addClass('hide-button');
     })
+    // Transforms heart buttons from outline to solid when clicked
+    $('.heart-button').click(function(){
+        $('.heart').toggleClass('fas fa-heart');
+        $('.heart').toggleClass('far fa-heart');
+    })
   });
-
-
-
-/*
-function showAllBooksBtn() {
-    btn = document.getElementById('#show-all-books-btn');
-    btn.classList.remove('hide-button');
-}
-
-function hideAllBooksButton() {
-    btn = document.getElementById('#show-all-books-btn');
-    btn.classList.add('hide-button');
-}*/
-
