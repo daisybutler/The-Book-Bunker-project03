6f3e8d04dc0708aa38fbe4782ba70ac7268a0d37
daisybutler <daisybutler16@gmail.com>
2021-03-11 15:25:38 +0000

    Added a back to top button to All Books page for good UX and styled.

--- static/js/script.js
+++ static/js/script.js
@@ -17,3 +17,26 @@ $(document).ready(function(){
         $(this).children('i').toggleClass('fas')
     }) */
 });
+
+
+
+// ------------------------ Uses an edited version of the code found at: https://codepen.io/deveb22/pen/QxPmGz
+
+// Hides or displays the #back-to-top-button depending on the user's scroll position (on All Books page only).
+
+var btn = $('#back-to-top-button');
+
+$(window).scroll(function () {
+	if ($(window).scrollTop() > 300) {
+		btn.addClass('show');
+	} else {
+		btn.removeClass('show');
+	}
+});
+
+btn.on('click', function (e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: 0
+	}, '300');
+});
