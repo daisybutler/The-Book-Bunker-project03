012a22f8b3077e06415ba97110d888f57dc63d8f
daisybutler <daisybutler16@gmail.com>
2021-03-14 11:40:01 +0000

    Positioned and styled a search bar button and applied to all instances of the searchbar.

--- static/js/script.js
+++ static/js/script.js
@@ -5,17 +5,6 @@ $(document).ready(function(){
     $('.fixed-action-btn').floatingActionButton();
     $('.tooltipped').tooltip();
 
-    /* Shows and hides the 'Show All Books' button on 'All Books' 
-    page depending on if the user has filtered results with search bar */
-    $('#show-all-books-btn').click(function(){
-        $('#show-all-books-btn').addClass('hide-button');
-    })
-    
-    /*
-    $('.bookmark').click(function(){
-        $(this).children('i').toggleClass('far')
-        $(this).children('i').toggleClass('fas')
-    }) */
 });
 
 
